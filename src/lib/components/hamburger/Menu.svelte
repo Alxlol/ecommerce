27f5424d031ef55bc785ac6debb4ba4b63a5a1fc
Alexander <friberg.alx@gmail.com>
2023-08-29 18:45:28 +0200

    fixed hamburger menu

--- src/lib/components/hamburger/Menu.svelte
+++ src/lib/components/hamburger/Menu.svelte
@@ -1,35 +1,38 @@
 <!-- Menu.svelte -->
 <script>
+	import { fly } from 'svelte/transition';
+
 	export let isOpen = false;
 	export let navlinks;
 	export let onClick;
 </script>
 
-<ul class="menu bg-white w-full text-center flex md:hidden" class:open={isOpen}>
-	{#each navlinks as navlink}
-		<li>
-			<a on:click={onClick} href={navlink.url}>{navlink.name}</a>
-		</li>
-		<hr class="my-2" />
-	{/each}
-</ul>
+{#key isOpen}
+	<ul
+		transition:fly={{ duration: 200, y: -80 }}
+		class="menu bg-white w-full text-center flex flex-col md:hidden"
+		class:open={isOpen}
+	>
+		{#each navlinks as navlink}
+			<li>
+				<a on:click={onClick} href={navlink.url}>{navlink.name}</a>
+			</li>
+			<hr class="my-2" />
+		{/each}
+	</ul>
+{/key}
 
 <style>
 	.menu {
 		display: none;
 		position: absolute;
-		bottom: 75%;
+		top: 70px;
 		left: 0;
 		z-index: 1;
-		box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-		transition: transform 0.3s ease-in-out;
-		transform-origin: bottom center;
-		transform: scaleY(0);
 	}
 
 	.open {
 		display: block;
-		transform: scaleY(1);
 	}
 
 	@media (min-width: 768px) {
